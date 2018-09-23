bb401caa26e413d556584fb8d1550623a7a94dd8
ductoanthanh <toanth@metropolia.fi>
2018-09-24 00:55:49 +0300

    Add tab bar & icons

--- app/src/main/java/fi/metropolia/harrytoan/gofitness/MainActivity.kt
+++ app/src/main/java/fi/metropolia/harrytoan/gofitness/MainActivity.kt
@@ -1,12 +1,47 @@
 package fi.metropolia.harrytoan.gofitness
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_songs -> {
+                toolbar.title = "Songs"
+                val songsFragment = SongsFragment.newInstance()
+                openFragment(songsFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                toolbar.title = "Dashboard"
+                val dashboardFragment = DashboardFragment.newInstance()
+                openFragment(dashboardFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
